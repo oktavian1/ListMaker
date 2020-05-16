7766aee722d9284e531c08875fdb9faff38c180b
oktavian1 <ilham.oktavian1313@gmail.com>
2020-05-16 19:24:37 +0700

    end of part1 create list

--- app/src/main/java/com/example/listmaker/MainActivity.kt
+++ app/src/main/java/com/example/listmaker/MainActivity.kt
@@ -5,20 +5,33 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        with(listRecyclerview){
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            this.adapter = TodoListAdapter()
+        }
+
+
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/example/listmaker/TodoListAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.listmaker
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.todo_list_view_holder.view.*
+
+class TodoListAdapter: RecyclerView.Adapter<TodoListAdapter.ListViewHolder>() {
+
+    private val todoList = arrayOf("Android Development", "House Work", "House 46")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.todo_list_view_holder, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = todoList.size
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        holder.listPositionTextView.text = (position +1).toString()
+        holder.listTitleTextView.text = todoList[position]
+    }
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var listPositionTextView = itemView.itemNumber
+        var listTitleTextView = itemView.itemString
+    }
+}
